30c62d8293881c102d9367d56063fe24d2f099b5
Naufal Fais Hakim <naufalfais21@gmail.com>
2021-02-10 01:45:52 +0700

    Update perkuliahan_controllers

--- application/controllers/Perkuliahan_controllers.php
+++ application/controllers/Perkuliahan_controllers.php
@@ -18,13 +18,13 @@ class Perkuliahan_controllers extends CI_Controller
         $data['list_fakultas'] = $this->fakultas_model->daftar_fakultas();
         $data['list_jurusan'] = $this->fakultas_model->daftar_jurusan();
         $data['list_jurusan2'] = $this->fakultas_model->daftar_jurusan();
-        $this->load->view("Perkuliahan/index", $data);
+        $this->load->view("perkuliahan/index", $data);
         if (isset($_POST['tambahMatkul'])) {
             $this->perkuliahan_model->tambahMatkul($_POST);
-            redirect("Perkuliahan_controllers");
+            redirect("perkuliahan_controllers");
         } else if (isset($_POST['tambahMkdu'])) {
             $this->perkuliahan_model->tambahMkdu($_POST);
-            redirect("Perkuliahan_controllers/matkulAmpu");
+            redirect("perkuliahan_controllers/matkulAmpu");
         }
     }
 
@@ -62,10 +62,10 @@ class Perkuliahan_controllers extends CI_Controller
         $data['my_profile']      = $this->dosen_model->exploredosenByNip($nip);
         $data['list_matkul']    = $this->perkuliahan_model->daftarMatkul();
         $data['matkul_ajarku'] = $this->perkuliahan_model->daftarMatkulByDosen($nip);
-        $this->load->view("Perkuliahan/matkul_ampu", $data);
+        $this->load->view("perkuliahan/matkul_ampu", $data);
         if (isset($_POST['ambilMatkul'])) {
             $this->perkuliahan_model->ambilMatkul($_POST, $nip);
-            redirect("/Perkuliahan_controllers/matkulAmpu");
+            redirect("/perkuliahan_controllers/matkulAmpu");
         }
     }
 
@@ -89,7 +89,7 @@ class Perkuliahan_controllers extends CI_Controller
             $data['list_matkul']    = $this->perkuliahan_model->daftarMatkul();
             $data['matkul_ajarku'] = $this->perkuliahan_model->daftarMatkulByDosen($nip);
             $data['data_matkul'] = $this->perkuliahan_model->daftarData_kuliah($nip);
-            $this->load->view("Perkuliahan/data_kuliah", $data);
+            $this->load->view("perkuliahan/data_kuliah", $data);
         } catch (Exception $e){
             show_error("Internal Server Error",500);
         }
@@ -100,13 +100,13 @@ class Perkuliahan_controllers extends CI_Controller
         $data['list_matkul'] = $this->perkuliahan_model->daftarMatkul();
         $data['list_mkdu'] = $this->perkuliahan_model->daftarMKDU();
         $data['list_fakultas'] = $this->fakultas_model->daftar_fakultas();
-        $this->load->view("Perkuliahan/mkdu_fakultas", $data);
+        $this->load->view("perkuliahan/mkdu_fakultas", $data);
         if (isset($_POST['tambahMatkul'])) {
             $this->perkuliahan_model->tambahMatkul($_POST);
-            redirect("Perkuliahan_controllers");
+            redirect("perkuliahan_controllers");
         } else if (isset($_POST['tambahMkdu'])) {
             $this->perkuliahan_model->tambahMkdu($_POST);
-            redirect("Perkuliahan_controllers/dataMKDU_fakultas");
+            redirect("perkuliahan_controllers/dataMKDU_fakultas");
         }
     }
 
