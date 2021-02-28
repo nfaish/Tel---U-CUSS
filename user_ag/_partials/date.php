a146dd92cd6fee84bd0ffb1c7a07e78e2ef377a1
Naufal Fais Hakim <naufalfais21@gmail.com>
2021-02-28 08:49:10 +0700

    Delete File yg Tidak Digunakan

--- user_ag/_partials/date.php
+++
@@ -1,2 +0,0 @@
-<script src="<?= base_url('assets/js/fecha.js') ?>"></script>
-<script src="<?= base_url('assets/js/hotel-datepicker.js') ?>"></script>
--- user_ag/_partials/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($this->session->userdata("user_role") == 0) {
-    $this->load->view("_partials/sidebar_admin.php");
-} else {
-    $this->load->view("_partials/sidebar_dsn.php");
-}
